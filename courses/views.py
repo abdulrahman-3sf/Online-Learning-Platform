84d8fd3b8c0d4647a9d273dbf04f23427dd62aad
Abdulrahman Alassaf. <163548732+abdulrahman-3sf@users.noreply.github.com>
2025-11-05 21:21:15 +0300

    change serializer.py to serializers.py & create register_view

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserRegisterationSerializer
 
-# Create your views here.
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register_view(request):
+    serializer = UserRegisterationSerializer(data=request.data)
+
+    if serializer.is_valid():
+        try:
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({'error': 'Faild to create user', 'detail': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
