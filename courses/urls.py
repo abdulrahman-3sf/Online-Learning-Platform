53a37190b506ef7057cc1b5d14e9cdcb91fdac0b
Abdulrahman Alassaf. <163548732+abdulrahman-3sf@users.noreply.github.com>
2025-11-05 23:57:29 +0300

    create profile_view & add urls for register, login, profile

--- courses/urls.py
+++ courses/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import register_view, login_view, profile_view
 
 urlpatterns = [
-    
+    path('auth/register/', register_view, name='register'),
+    path('auth/login/', login_view, name='login'),
+    path('auth/profile/', profile_view, name='profile'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,5 @@
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -15,9 +15,9 @@ def register_view(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         except Exception as e:
-            return Response({'error': 'Faild to create user', 'detail': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response({'error': 'Failed to create user', 'detail': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -39,3 +39,27 @@ def login_view(request):
         return Response(response_data, status=status.HTTP_200_OK)
     
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'PATCH'])
+@permission_classes([IsAuthenticated])
+def profile_view(request):
+    if request.method == 'GET':
+        user = request.user
+
+        serializer = UserSerializer(user)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    if request.method in ['PUT', 'PATCH']:
+        is_partial = request.method == 'PATCH'
+
+        user = request.user
+
+        serializer = UserSerializer(user, data=request.data, partial=is_partial)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
