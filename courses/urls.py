33e78d5eac8cb9d582f998c0f7dd3d0faaba8c5b
Abdulrahman Alassaf. <163548732+abdulrahman-3sf@users.noreply.github.com>
2025-11-08 23:28:16 +0300

    Setup urls with routers

--- courses/urls.py
+++ courses/urls.py
@@ -1,8 +1,25 @@
-from django.urls import path
-from .views import register_view, login_view, profile_view
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    register_view, 
+    login_view, 
+    profile_view,
+    CategoryViewSet,
+    CourseViewSet,
+    ModuleViewSet,
+    LessonViewSet
+)
+
+router = DefaultRouter()
+
+router.register('categories', CategoryViewSet, basename='category')
+router.register('courses', CourseViewSet, basename='course')
+router.register('modules', ModuleViewSet, basename='module')
+router.register('lessons', LessonViewSet, basename='lesson')
 
 urlpatterns = [
     path('auth/register/', register_view, name='register'),
     path('auth/login/', login_view, name='login'),
     path('auth/profile/', profile_view, name='profile'),
+    path('', include(router.urls)),
 ]
