5d833e09045c5ccea46e916b8797481e35fdd69a
Abdulrahman Alassaf. <163548732+abdulrahman-3sf@users.noreply.github.com>
2025-11-08 22:07:28 +0300

    create IsInstructorOrReadOnly permission

--- courses/views.py
+++ courses/views.py
@@ -63,3 +63,4 @@ def profile_view(request):
             return Response(serializer.data, status=status.HTTP_200_OK)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
