61af567bca5d70ff111230ef82dc1585b7e227f9
Abdulrahman Alassaf. <163548732+abdulrahman-3sf@users.noreply.github.com>
2025-11-10 19:21:04 +0300

    add validation for file type

--- courses/validators.py
+++ courses/validators.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ValidationError
+import os
 
 def validate_file_size(file):
     max_size_mb = 5
@@ -7,3 +8,16 @@ def validate_file_size(file):
     if file.size > max_size_bytes:
         raise ValidationError(f'File size must be less than {max_size_mb}MB!')
 
+def validate_image_file(file):
+    ext = os.path.splitext(file.name)[1].lower()
+
+    valid_extensions = ['.png', '.jpg', '.jpeg']
+
+    if ext not in valid_extensions:
+        raise ValidationError(f'Invalid file type. Allowed: {" ".join(valid_extensions)}')
+
+def validate_document_file(file):
+    pass
+
+def validate_video_url(value):
+    pass
